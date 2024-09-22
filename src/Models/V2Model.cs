41a1cd443f7e70fefdade77d6de57be2c2eb5d24
Wargencob <145149664+Wargencob@users.noreply.github.com>
2024-09-22 22:31:19 +0300

    Add files via upload

---
+++ src/Models/V2Model.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ДЗ1.Models
+{
+    internal class V2Model
+    {
+        public int id { get; set; }
+        public double result { get; set; }
+    }
+}
